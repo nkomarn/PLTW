437ee7bfe6d903916c77cbad98a7f80a35fd62e6
Jacob Cunningham <jacob.joc@gmail.com>
2018-01-18 21:51:22 -0800

    Add files via upload

--- PLTW1.5.1/PLTW1.5.1/Form1.Designer.cs
+++ PLTW1.5.1/PLTW1.5.1/Form1.Designer.cs
@@ -33,8 +33,22 @@
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -51,18 +65,172 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(1105, 616);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Help";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(218, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(715, 488);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(909, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Language";
+            this.label1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(255, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "English";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(255, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Spanish";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(255, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "German";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(255, 181);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Russian";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(382, 550);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 621);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(394, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(525, 514);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 33);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Top Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(507, 585);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 33);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Bottom Text";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(464, 450);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(226, 42);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Apply";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1192, 653);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +239,19 @@
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- PLTW1.5.1/PLTW1.5.1/Form1.cs
+++ PLTW1.5.1/PLTW1.5.1/Form1.cs
@@ -14,10 +14,15 @@ namespace PLTW1._5._1
     {
         public Form1()
         {
+            
+
             InitializeComponent();
+            
             pictureBox1.MouseDoubleClick += new MouseEventHandler(pickPic);
-
-
+            button1.MouseClick += new MouseEventHandler(helpMenu);
+            button2.MouseClick += new MouseEventHandler(helpMenuClose);
+            button7.MouseClick += new MouseEventHandler(applyText);
+            pic = (Bitmap)pictureBox1.Image;
         }
         Bitmap pic;
         System.IO.StreamReader sr;
@@ -41,6 +46,156 @@ namespace PLTW1._5._1
             }
 
             
+        }
+
+        public void helpMenu(object sender, MouseEventArgs e) {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = true;
+            pictureBox2.Visible = true;
+            button2.Enabled = true;
+            button2.Visible = true;
+            button3.Enabled = true;
+            button3.Visible = true;
+            button4.Enabled = true;
+            button4.Visible = true;
+            button5.Enabled = true;
+            button5.Visible = true;
+            button6.Enabled = true;
+            button6.Visible = true;
+            label1.Enabled = true;
+            label1.Visible = true;
+            label1.BringToFront();
+            button2.BringToFront();
+            button3.BringToFront();
+            button4.BringToFront();
+            button5.BringToFront();
+            button6.BringToFront();
+
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            button7.Enabled = false;
+
+            button3.MouseClick += new MouseEventHandler(English);
+            button4.MouseClick += new MouseEventHandler(Spanish);
+            button5.MouseClick += new MouseEventHandler(German);
+            button6.MouseClick += new MouseEventHandler(Russian);
+        }
+        public void helpMenuClose(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Enabled = true;
+            pictureBox2.Visible = false;
+            button2.Visible = false;
+            pictureBox2.Enabled = false;
+            label1.Enabled = false;
+            label1.Visible = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button3.Visible = false;
+            button4.Enabled = false;
+            button4.Visible = false;
+            button5.Enabled = false;
+            button5.Visible = false;
+            button6.Enabled = false;
+            button6.Visible = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button7.Enabled = true;
+        }
+        public int language;
+        public void English(object sender, MouseEventArgs e) {
+            language = 1;
+            languageSwitch();
+        }
+        public void Spanish(object sender, MouseEventArgs e)
+        {
+            language = 2;
+            languageSwitch();
+        }
+        public void German(object sender, MouseEventArgs e)
+        {
+            language = 3;
+            languageSwitch();
+        }
+        public void Russian(object sender, MouseEventArgs e)
+        {
+            language = 4;
+            languageSwitch();
+        }
+        public void languageSwitch() {
+            switch (language)
+            {
+                default:
+                    button1.Text = "Help";
+                    button3.Text = "English";
+                    button4.Text = "Spanish";
+                    button5.Text = "German";
+                    button6.Text = "Russian";
+                    label1.Text = "Language";
+                    label2.Text = "Top Text";
+                    label3.Text = "Bottom Text";
+                    button7.Text = "Apply";
+                    break;
+                case 1:
+                    button1.Text = "Help";
+                    button3.Text = "English";
+                    button4.Text = "Spanish";
+                    button5.Text = "German";
+                    button6.Text = "Russian";
+                    label1.Text = "Language";
+                    label2.Text = "Top Text";
+                    label3.Text = "Bottom Text";
+                    button7.Text = "Apply";
+                    break;
+                case 2:
+                    button1.Text = "Ayuda";
+                    button3.Text = "Inglés";
+                    button4.Text = "Español";
+                    button5.Text = "Alemán";
+                    button6.Text = "Ruso";
+                    label1.Text = "Idioma";
+                    label2.Text = "Texto superior";
+                    label3.Text = "Texto inferior";
+                    button7.Text = "Aplicar";
+                    break;
+                case 3:
+                    button1.Text = "Hilfe";
+                    button3.Text = "Englisch";
+                    button4.Text = "Spanisch";
+                    button5.Text = "Deutsche";
+                    button6.Text = "Russisch";
+                    label1.Text = "Sprache";
+                    label2.Text = "Top-text";
+                    label3.Text = "Fußzeile";
+                    button7.Text = "Sich bewerben";
+                    break;
+                case 4:
+                    button1.Text = "Помогите";
+                    button3.Text = "английский";
+                    button4.Text = "испанский";
+                    button5.Text = "Немецкий";
+                    button6.Text = "русский";
+                    label1.Text = "язык";
+                    label2.Text = "Заголовок";
+                    label3.Text = "Нижний текст";
+                    button7.Text = "Подать заявление";
+                    break;
+
+
+            }
+        }
+        Graphics g;
+        RectangleF top = new RectangleF(5, 25, 650, 120);
+        RectangleF bottom = new RectangleF(5, 450, 650, 120);
+        Bitmap pic2;
+        public void applyText(object sender, MouseEventArgs e) {
+            pic2 = (Bitmap)pictureBox1.Image;
+            g = Graphics.FromImage(pic);
+            
+            g.DrawString(textBox1.Text, new Font("Palatino Linotype", 100), Brushes.White, top);
+            g.DrawString(textBox2.Text, new Font("Palatino Linotype", 100), Brushes.White, bottom);
+            g.Flush();
+
+            pictureBox1.Image = pic;
         }
     }
 }
